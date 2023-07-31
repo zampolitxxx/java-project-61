c023b5ed87d8e058d486deea8118e7c4dc5d7714
zampolitxxx <zampolitxxx@gmail.com>
2023-07-31 23:41:12 +0900

    add apache.commmons.lang3 and fixed linter comments
    added apache.commons.lang3.ArrayUtils library to remove magic numbers in case. Fixed linter comments

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,11 @@
 package hexlet.code;
+import org.apache.commons.lang3.ArrayUtils;
 
 public class App {
-    private static final int NumberOfRounds = 3;
+    private static final int NUMBER_OF_ROUNDS = 3;
     private static int score = 0;
-    static private final String CorrectAnswer = "Correct!";
+    private static final int NUMBER_OF_QESTION_IN_STRUCT = 2;
+    private static final String CORRECT_ANSWER = "Correct!";
     public static void main(String[] args) {
         String[][] games = {
                 {"0", "Exit", ""},
@@ -15,15 +17,19 @@ public class App {
                 {"6", "Prime", "Answer 'yes' if given number is prime. Otherwise answer 'no'."}
         };
         int choice = Engine.getGameNumber(games);
-        String result = ""; // Если игрок выиграл раунд, возвращается строка "Correct!", иначе выводим строку с правильным ответом и предлагаем сыграть еще раз
+        /*
+            If the player has won the round, the string "Correct!" is returned,
+            otherwise we output the string with the correct answer and offer to play again
+         */
+        String result = "";
 
-        if (choice == 1) {
+        if (choice == ArrayUtils.indexOf(games, "Greet")) {
             Cli.greeting();
         }
 
         if (choice > 1 & choice < games.length) {
             Cli.greeting();
-            Engine.printMessage(games[choice][2]);
+            Engine.printMessage(games[choice][NUMBER_OF_QESTION_IN_STRUCT]);
             do {
                 switch (choice) {
                     case 2: result = Game.playParityNumber(); break;
@@ -31,18 +37,19 @@ public class App {
                     case 4: result = Game.playGCD(); break;
                     case 5: result = Game.playProgression(); break;
                     case 6: result = Game.playPrime(); break;
+                    default: result = "Your choice is not correct";
                 }
-                if (result.equals(CorrectAnswer)) {
-                    Engine.printMessage(CorrectAnswer);
+                if (result.equals(CORRECT_ANSWER)) {
+                    Engine.printMessage(CORRECT_ANSWER);
                     score++;
                 } else {
                     Engine.printMessage(result);
                     Engine.printMessage("Let's try again, " + Cli.getName() + "!");
                     break;
                 }
-            } while (score < NumberOfRounds);
+            } while (score < NUMBER_OF_ROUNDS);
         }
-        if (score == NumberOfRounds) {
+        if (score == NUMBER_OF_ROUNDS) {
             Engine.printMessage("Congratulations, " + Cli.getName() + "!");
         }
     }
--- app/src/main/java/hexlet/code/Game.java
+++ app/src/main/java/hexlet/code/Game.java
@@ -2,12 +2,12 @@ package hexlet.code;
 import java.lang.Math;
 
 public class Game {
-    private static final int MinRandom = 1;
-    private static final int MaxRandom = 20;
+    private static final int MIN_RANDOM = 1;
+    private static final int MAX_RANDOM = 20;
 
 
     public static String playParityNumber() {
-            int randomNumber = 1 + (int) (Math.random() * (MaxRandom - MinRandom) + MinRandom);
+            int randomNumber = 1 + (int) (Math.random() * (MAX_RANDOM - MIN_RANDOM) + MIN_RANDOM);
             Engine.printMessage("Question: " + randomNumber);
             String answer = Engine.getStr("Your answer: ");
             if ((answer.equals("yes") && (randomNumber % 2) == 0) || (answer.equals("no") && (randomNumber % 2) != 0)) {
@@ -25,8 +25,8 @@ public class Game {
         int usersResult = 0;
         int result = 0;
         char[] sign = {'+', '-', '*'};
-        int firstNumber = 1 + (int) (Math.random() * (MaxRandom - MinRandom) + MinRandom);
-        int secondNumber = 1 + (int) (Math.random() * (MaxRandom - MinRandom) + MinRandom);
+        int firstNumber = 1 + (int) (Math.random() * (MAX_RANDOM - MIN_RANDOM) + MIN_RANDOM);
+        int secondNumber = 1 + (int) (Math.random() * (MAX_RANDOM - MIN_RANDOM) + MIN_RANDOM);
         char signInExpression = sign[((int) (Math.random() * (sign.length)))];
         Engine.printMessage("Question: " + firstNumber + " " + signInExpression + " " + secondNumber);
         usersResult = Engine.getNumber("Your answer: ");
@@ -34,6 +34,7 @@ public class Game {
             case '-': result = firstNumber - secondNumber; break;
             case '+': result = firstNumber + secondNumber; break;
             case '*': result = firstNumber * secondNumber; break;
+            default: result = 0;
         }
         if (usersResult == result) {
             return "Correct!";
@@ -42,8 +43,8 @@ public class Game {
         }
     }
     public static String playGCD() {
-        int firstNumber = 1 + (int) (Math.random() * (MaxRandom - MinRandom) + MinRandom);
-        int secondNumber = 1 + (int) (Math.random() * (MaxRandom - MinRandom) + MinRandom);
+        int firstNumber = 1 + (int) (Math.random() * (MAX_RANDOM - MIN_RANDOM) + MIN_RANDOM);
+        int secondNumber = 1 + (int) (Math.random() * (MAX_RANDOM - MIN_RANDOM) + MIN_RANDOM);
         Engine.printMessage("Question: " + firstNumber + " " + secondNumber);
         int usersResult = Engine.getNumber("Your answer: ");
         int result = Engine.getGCD(firstNumber, secondNumber);
@@ -56,7 +57,7 @@ public class Game {
 
     public static String playProgression() {
         final int progressionLength = 10;
-        int sizeStringbuffer = 3;           // Коэффициент для выделения памяти под stringbuffer
+        final int sizeStringbuffer = 3;           // Коэффициент для выделения памяти под stringbuffer
         StringBuffer sb = new StringBuffer(progressionLength * sizeStringbuffer);
         int indexOfHiddenNumber = (int) (Math.random() * progressionLength);
         int[] progressionOfNumbers = Engine.getProgression("sum", progressionLength);
@@ -78,13 +79,12 @@ public class Game {
 
     public static String playPrime() {
         int shiftNumber = 1;   //avoids one in a number of random numbers
-        int randomNumber = shiftNumber + (int) (Math.random() * (MaxRandom - MinRandom) + MinRandom);
+        int randomNumber = shiftNumber + (int) (Math.random() * (MAX_RANDOM - MIN_RANDOM) + MIN_RANDOM);
         Engine.printMessage("Question: " + randomNumber);
         String userResponse = Engine.getStr("Your answer: ");
         if ((userResponse.equals("yes") & Engine.isPrime(randomNumber)) || (userResponse.equals("no") & !Engine.isPrime(randomNumber))) {
             return "Correct!";
-        }
-        else {
+        } else {
             if (Engine.isPrime(randomNumber)) {
                 return "'" + userResponse + "'" + " is wrong answer ;(. Correct answer was 'yes'.";
             } else {
