14073e29b6e816a3d6547347fa4a18957e4fa4ff
zampolitxxx <zampolitxxx@gmail.com>
2023-08-13 18:56:11 +0900

    fix bug in Even.java

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -6,8 +6,10 @@ import hexlet.code.Util;
 public class Even {
     private static final String RULES = "Answer 'yes' if the number is even, otherwise answer 'no'.";
     private static final int MAX_RANDOM = 20;
-    private static final String[][] DATA = new String[Engine.NUMBER_OF_ROUNDS][2];
+    private static final int MIN_RANDOM = 1;
+
     public static void playEven() {
+        final String[][] DATA = new String[Engine.NUMBER_OF_ROUNDS][2];
         for (int i = 0; i < DATA.length; i++) {
             DATA[i] = getRoundData();
         }
@@ -15,13 +17,13 @@ public class Even {
     }
 
     private static String[] getRoundData(){
-        int randomNumber = Util.getRandom(MAX_RANDOM);
-        String correctAnswer = isPrime(randomNumber) ? "yes" : "no";
+        int randomNumber = Util.getRandom(MAX_RANDOM, MIN_RANDOM);
+        String correctAnswer = isEven(randomNumber) ? "yes" : "no";
         String question = Integer.toString(randomNumber);
         return new String[] {question, correctAnswer};
     }
 
-    private static boolean isPrime(int number) {
+    private static boolean isEven(int number) {
         return number % 2 == 0;
     }
 }
