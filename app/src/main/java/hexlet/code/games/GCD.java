bb14d2f88a3f83b72c001548f32ca37c0a520466
zampolitxxx <zampolitxxx@gmail.com>
2023-08-13 18:56:46 +0900

    fix bug in GCD.java

--- app/src/main/java/hexlet/code/games/GCD.java
+++ app/src/main/java/hexlet/code/games/GCD.java
@@ -1,25 +1,28 @@
 package hexlet.code.games;
 
 import hexlet.code.Engine;
+import hexlet.code.Util;
 
 public class GCD {
     private static final String RULES = "Find the greatest common divisor of given numbers.";
     private static final int MAX_RANDOM = 20;
-    private static final int MIN_RANDOM = 1;
-    private static String[][] data = new String[Engine.NUMBER_OF_ROUNDS][2];
-    private static int correctResult = 0;
-    private static String correctAnswer = "";
+    private static final String[][] DATA = new String[Engine.NUMBER_OF_ROUNDS][2];
+
     public static void playGCD() {
-        for (int i = 0; i < data.length; i++) {
-            int firstNumber = 1 + (int) (Math.random() * (MAX_RANDOM - MIN_RANDOM) + MIN_RANDOM);
-            int secondNumber = 1 + (int) (Math.random() * (MAX_RANDOM - MIN_RANDOM) + MIN_RANDOM);
-            correctResult = getGCD(firstNumber, secondNumber);
-            correctAnswer = Integer.toString(correctResult);
-            data[i][0] = firstNumber + " " + secondNumber;
-            data[i][1] = correctAnswer;
+        for (int i = 0; i < DATA.length; i++) {
+            DATA[i] = getRoundData();
         }
-        Engine.startGame(RULES, data);
+        Engine.startGame(RULES, DATA);
     }
+
+    private static String[] getRoundData(){
+        int firstNumber = Util.getRandom(MAX_RANDOM);
+        int secondNumber = Util.getRandom(MAX_RANDOM);
+        String correctAnswer = Integer.toString(getGCD(firstNumber, secondNumber));
+        String question = firstNumber + " " + secondNumber;
+        return new String[] {question, correctAnswer};
+    }
+
     private static int getGCD(int a, int b) {
         int remainder = 0;
         int smallerNumber = a;
