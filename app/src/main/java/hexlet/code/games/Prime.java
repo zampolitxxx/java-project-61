679d00d2f08d00ef9e44b89dc70037e2faf26307
zampolitxxx <zampolitxxx@gmail.com>
2023-08-13 18:57:15 +0900

    fix bug in Prime.java

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -5,23 +5,30 @@ import hexlet.code.Util;
 
 public class Prime {
     private static final String RULES = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
+    private static final String[][] DATA = new String[Engine.NUMBER_OF_ROUNDS][2];
     private static final int MAX_RANDOM = 20;
-    private static String[][] data = new String[Engine.NUMBER_OF_ROUNDS][2];
+    private static final int MIN_RANDOM = 1;
 
     public static void playPrime() {
-        for (int i = 0; i < data.length; i++) {
-            data[i] = getRoundData();
+        for (int i = 0; i < Engine.NUMBER_OF_ROUNDS; i++) {
+            DATA[i] = getRoundData();
         }
+        Engine.startGame(RULES, DATA);
     }
 
     private static String[] getRoundData(){
-        int randomNumber = Util.getRandom(MAX_RANDOM);
-        String correctAnswer = isPrime(randomNumber) ? "yes" : "no";
+        int randomNumber = Util.getRandom(MAX_RANDOM, MIN_RANDOM);
         String question = Integer.toString(randomNumber);
+        String correctAnswer = isPrime(randomNumber) ? "yes" : "no";
         return new String[] {question, correctAnswer};
     }
-    
+
     private static boolean isPrime(int number) {
-        return number % 2 == 0;
+        for (int i = 2; i <= number / 2; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
