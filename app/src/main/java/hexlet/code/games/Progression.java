b37e606d78c5fa44c8e6275c58935a83979c349e
zampolitxxx <zampolitxxx@gmail.com>
2023-08-13 19:00:23 +0900

    add method getRounData
    now getProgression function for generate progression only
    getRoundData function for the rest of the work

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -1,38 +1,36 @@
 package hexlet.code.games;
 
 import hexlet.code.Engine;
+import hexlet.code.Util;
+
+import java.util.Arrays;
 
 public class Progression {
-    private static final int PROGRESSION_LENGTH = 10;
-    private static final int SIZE_STRINGBUFFER = 3;           // coefficient for memory allocation for StringBuffer
     public static final String RULES = "What number is missing in the progression?";
-    private static String[][] data = new String[Engine.NUMBER_OF_ROUNDS][2];
-    private static int correctResult = 0;
-    private static String correctAnswer = "";
+    private static final int PROGRESSION_LENGTH = 10;
+    private static final String[][] DATA = new String[Engine.NUMBER_OF_ROUNDS][2];
+
     public static void playProgression() {
-        for (int i = 0; i < data.length; i++) {
-            StringBuffer sb = new StringBuffer(PROGRESSION_LENGTH * SIZE_STRINGBUFFER);
-            int indexOfHiddenNumber = (int) (Math.random() * PROGRESSION_LENGTH);
-            correctResult = indexOfHiddenNumber;
-            correctAnswer = Integer.toString(correctResult);
-            int[] progressionOfNumbers = getProgression(PROGRESSION_LENGTH);
-            for (int j = 0; j < PROGRESSION_LENGTH; j++) {
-                if (j == indexOfHiddenNumber) {
-                    sb.append(".. ");
-                } else {
-                    sb.append(progressionOfNumbers[j] + " ");
-                }
-            }
-            data[i][0] = sb.toString();
-            data[i][1] = correctAnswer;
+        for (int i = 0; i < DATA.length; i++) {
+            DATA[i] = getRoundData();
         }
-        Engine.startGame(RULES, data);
+        Engine.startGame(RULES, DATA);
     }
-    private static int[] getProgression(int progressionLength) {
-        int[] progressionNumbers = new int[progressionLength];
+
+    private static String[] getRoundData() {
+        int indexOfHiddenNumber = Util.getRandom(PROGRESSION_LENGTH);
+        String correctAnswer = Integer.toString(indexOfHiddenNumber);
+        String[] arrQuestion = getProgression(PROGRESSION_LENGTH);      //["0", "1", "2", "3", ...]
+        arrQuestion[indexOfHiddenNumber] = "..";    //["0", "..", "2", "3", ...]
+        String question = String.join(",", arrQuestion).replace(',', ' ');  //"0 .. 2 3...."
+        return new String[] {question, correctAnswer};
+    }
+
+    private static String[] getProgression(int progressionLength) {
+        String[] progressionNumbers = new String[progressionLength];
         for (int i = 0; i < progressionLength; i++) {
-            progressionNumbers[i] = i;
+            progressionNumbers[i] = Integer.toString(i);
         }
-        return progressionNumbers;
+        return progressionNumbers;  //["0", "1", "2", "3", ...]
     }
 }
