271e6329bbe1bc53f6b9f08255f19d1fc6944e99
zampolitxxx <zampolitxxx@gmail.com>
2023-08-13 18:53:23 +0900

    add constructor to Util.java
    add constructor with 2 arguments
    getRandom(..., int minRandom)

--- app/src/main/java/hexlet/code/Util.java
+++ app/src/main/java/hexlet/code/Util.java
@@ -1,9 +1,18 @@
 package hexlet.code;
 
 public class Util {
+
     private static int minRandom = 0;
+
+//    generates values from minRandom to maxRandom
+    public static int getRandom(int maxRandom, int minRandom) {
+        int randomNumber = (int) (Math.random() * (maxRandom - minRandom) + minRandom);
+        return randomNumber;
+    }
+
+//    generates values from zero to maxRandom
     public static int getRandom(int maxRandom) {
-        int randomNumber = 1 + (int) (Math.random() * (maxRandom - minRandom) + minRandom);
+        int randomNumber = (int) (Math.random() * (maxRandom - minRandom) + minRandom);
         return randomNumber;
     }
 }
