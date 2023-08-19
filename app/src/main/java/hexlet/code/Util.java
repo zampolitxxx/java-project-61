f51c1fabf52eb4f9edd68bb7982ed30f51be1335
zampolitxxx <zampolitxxx@gmail.com>
2023-08-19 17:49:44 +0900

    remove calculate mathod to Calc.java

--- app/src/main/java/hexlet/code/Util.java
+++ app/src/main/java/hexlet/code/Util.java
@@ -5,16 +5,4 @@ public class Util {
     public static int getRandom(int maxRandom, int minRandom) {
         return (int) (Math.random() * (maxRandom - minRandom) + minRandom);
     }
-
-    public static int calculate(int firstNumber, int sign, int secondNumber) {
-        switch (sign) {
-            case 0:
-                return firstNumber + secondNumber;
-            case 1:
-                return firstNumber - secondNumber;
-            case 2:
-                return firstNumber * secondNumber;
-            default: return 0;
-        }
-    }
 }
